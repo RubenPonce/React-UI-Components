f75a16b640c47f7f768c4e89314845f329d8494b
Ruben Ponce <ruben_ponce1@yahoo.com>
2019-04-22 15:44:49 -0700

    somehwat finished with react twitter post

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = ()=>{
+    return(
+        <div className ="footer">
+            <i className="far fa-comment icon-rotate-180"></i>
+            <i className="fas fa-retweet"></i>
+            <i className="far fa-heart"></i>
+            <i className="far fa-envelope"></i>
+        </div>
+    )
+}
+export default Footer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+import logo from './lambda-logo.jpg'
+const ImageThumbnail = ()=>{
+    return (
+        <img className="thumbnail" src={logo} alt="lambda school crest"></img>
+    )
+}
+
+export default ImageThumbnail;
