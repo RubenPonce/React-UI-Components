1d9b0ddc6ab1b2ed6ab5ecb4be238b965210a560
Ruben Ponce <ruben_ponce1@yahoo.com>
2019-04-23 13:50:36 -0700

    laid out most buttons. just need to fix display and add functionality

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,47 @@
 import React from 'react';
-import './App.css';
-
+import './App.scss';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
 const App = () => {
   return (
     <div>
       <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+      <div className="app">
+        <CalculatorDisplay />
+        <div className="button-container">
+          <div className="left">
+            <ActionButton action="clear" className="wide-btn" />
+            <div className="numbers">
+              <div className="row">
+                <NumberButton text="7" />
+                <NumberButton text="8" />
+                <NumberButton text="9" />
+              </div>
+              <div className="row">
+                <NumberButton text="4" />
+                <NumberButton text="5" />
+                <NumberButton text="6" />
+              </div>
+              <div className="row">
+                <NumberButton text="1" />
+                <NumberButton text="2" />
+                <NumberButton text="3" />
+              </div>
+            </div>
+            <ActionButton action="0" className="wide-btn" />
+          </div>
+          <div className="right">
+            <ActionButton action="divide" className="btn" />
+            <ActionButton action="multiply" className="btn" />
+            <ActionButton action="subtract" className="btn" />
+            <ActionButton action="add" className="btn" />
+            <ActionButton action="=" className="btn" />
+          </div>
+
+        </div>
+      </div>
+
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,9 @@
 import React from 'react';
-import './Button.css';
+import './Button.scss';
+
+const ActionButton = (props)=>{
+    return(
+        <button className={props.className}>{props.action}</button>
+    )
+}
+export default ActionButton
